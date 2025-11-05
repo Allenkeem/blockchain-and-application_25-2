5fbd83d4d23470ed379a0c67cf339a791b4e0e16
Allenkeem <kc_w@naver.com>
2025-11-05 21:46:49 +0900

    추가

---
+++ week5/Ex5_12.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Ex5_12{
+
+    //uint public constant num1;
+    //uint[] public immutable arr;
+    uint public immutable num2;
+
+    constructor(uint _num){
+        num2 = _num;
+    }
+}
